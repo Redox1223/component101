07ec3c24f63d0c5838b6f64e35b0399e0ac5f920
Sasanka <sajewsasanka1@gmail.com>
2023-03-17 23:12:49 +0530

    Update mortion_sensor.ino

--- MotionSensor/mortion_sensor/mortion_sensor.ino
+++ MotionSensor/mortion_sensor/mortion_sensor.ino
@@ -1,26 +1,29 @@
 
 
+const int PIN_TO_SENSOR = 2; // the pin that OUTPUT pin of sensor is connected to
+int pinStateCurrent = LOW;   // current state of pin
+int pinStatePrevious = LOW;  // previous state of pin
 
-const int PIN_TO_SENSOR = 2; // the pin that OUTPUT pin of sensor is connected to 
-int pinStateCurrent = LOW; // current state of pin
-int pinStatePrevious = LOW; // previous state of pin
-
-void setup (){
-    serial.begin(9600);  //initialize serial
+void setup()
+{
+    serial.begin(9600);            // initialize serial
     pinMode(PIN_TO_SENSOR, INPUT); // configure an Arduino's pin to the digital input mode by using pinMode() function
-
 }
 
-void loop(){
-    pinStatePrevious = pinStateCurrent;// store old state
-    pinStateCurrent =digitalRead(PIN_TO_SENSOR);// Read state of sensor's OUTPUT pin by using digitalRead() funtion.
+void loop()
+{
+    pinStatePrevious = pinStateCurrent;           // store old state
+    pinStateCurrent = digitalRead(PIN_TO_SENSOR); // Read state of sensor's OUTPUT pin by using digitalRead() funtion.
 
-    if (pinStatePrevious== LOW && pinStateCurrent== HIGH){ //pin state change: LOW-->HIGH
+    // if the state of the sensor's OUTPUT pin has changed, print out the result:
+    if (pinStatePrevious == LOW && pinStateCurrent == HIGH)
+    {
         serial.println("Mortion detected!");
-        // TODO: turn on alarm, light or activate a device... here
-    } 
-    else if(pinStatePrevious== HIGH && pinStateCurrent==LOW){// pin state change:HIGH-->LOw
+    }
+
+    // if the state of the sensor's OUTPUT pin has changed, print out the result:
+    else if (pinStatePrevious == HIGH && pinStateCurrent == LOW)
+    {
         serial.println("Mortion stopped!");
-        // TODO: turn off alarm,light or deactivate a device...here    }
     }
 }
