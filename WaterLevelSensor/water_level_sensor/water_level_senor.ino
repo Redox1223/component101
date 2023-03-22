6ad1c5c903f209045bf4d1056c339ff54de3846b
RPSTD <120778675+RPSTD@users.noreply.github.com>
2023-03-22 13:10:25 +0530

    Update water_level_senor.ino

---
+++ WaterLevelSensor/water_level_sensor/water_level_senor.ino
@@ -0,0 +1,38 @@
+// Sensor Pins
+#define sensorPower 7
+#define sensorPin A0
+
+// Value for storing water level
+int val =0; 
+
+void setup(){ 
+    // Set D7 as an OUTPUT
+    pinout (sensorPower,LOW);
+    // Set to LOW so no power flows through the sensor
+    digitalWrite(sensorPower,LOW);
+
+    Serial.begin(9600);
+
+}
+
+void loop (){
+// get the reading from the funtion below and print it
+int level = readSensor();
+
+Serial.print("Water Level:");
+Serial.print(level);
+
+delay(1000);
+
+}
+
+// This is a funtion used to get the reading
+int readSensor()
+{
+    digitalWrite(sensorPower,HIGH);// Turn the sensor ON'
+    delay(10); // wait 10 miliseconds
+    val= analogRead(sensorPin);// Read the analog value from sensor
+    digitalWrite(sensorPower,LOW);//Turn the sensor OFF
+    return val;// Send current reading
+
+}
