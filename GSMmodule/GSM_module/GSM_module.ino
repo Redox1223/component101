7dd170f325f7f15c3428bdacd4b531eee8ae9c6d
RPSTD <120778675+RPSTD@users.noreply.github.com>
2023-03-29 22:09:35 +0530

    Update GSM_module.ino
    - Update GSM module code

--- GSMmodule/GSM_module/GSM_module.ino
+++ GSMmodule/GSM_module/GSM_module.ino
@@ -4,42 +4,61 @@ SoftwareSerial mySerial(9, 10);
 
 void setup()
 {
-    mySerial.begin(9600); // Setting the baud rate of GSM Module
-    Serial.begin(9600);   // Setting the baud rate of Serial Monitor (Arduino)
-    delay(100);
+
+    mySerial.begin(9600); // Initialize the software serial communication with the GSM module
+    Serial.begin(9600);   // Initialize the serial communication with the Arduino IDE serial monitor
+
+    delay(100); // Delay for 100ms
 }
 
 void loop()
 {
-    if (Serial.available() > 0)
-        switch (Serial.read())
+
+    if (Serial.available() > 0) // Check if there is data available in the serial monitor
+
+        switch (Serial.read()) // Read the input character from the serial monitor
         {
-        case 's':
-            SendMessage();
+
+        case 's':          // If the input character is 's'
+            SendMessage(); // Call the SendMessage function
             break;
-        case 'r':
-            RecieveMessage();
+
+        case 'r': // If the input character is 'r'
+
+            RecieveMessage(); // Call the RecieveMessage function
             break;
         }
 
-    if (mySerial.available() > 0)
-        Serial.write(mySerial.read());
+    if (mySerial.available() > 0) // Check if there is data available in the software serial buffer
+
+        Serial.write(mySerial.read()); // Read the data from the software serial buffer and send it to the serial monitor
 }
 
 void SendMessage()
 {
-    mySerial.println("AT+CMGF=1");                   // Sets the GSM Module in Text Mode
-    delay(1000);                                     // Delay of 1000 milli seconds or 1 second
+
+    mySerial.println("AT+CMGF=1"); // Set the GSM module to text mode
+
+    delay(1000); // Delay for 1 second
+
+    // Set the recipient's mobile number
     mySerial.println("AT+CMGS=\"+91xxxxxxxxxx\"\r"); // Replace x with mobile number
-    delay(1000);
-    mySerial.println("I am SMS from GSM Module"); // The SMS text you want to send
-    delay(100);
-    mySerial.println((char)26); // ASCII code of CTRL+Z
-    delay(1000);
+
+    delay(1000); // Delay for 1 second
+
+    mySerial.println("I am SMS from GSM Module"); // Set the message to be sent
+
+    delay(100); // Delay for 100ms
+
+    mySerial.println((char)26); // Send the ASCII code of CTRL+Z to indicate the end of the message
+
+    delay(1000); // Delay for 1 second
 }
 
 void RecieveMessage()
 {
-    mySerial.println("AT+CNMI=2,2,0,0,0"); // AT Command to receive a live SMS
-    delay(1000);
+
+    mySerial.println("AT+CNMI=2,2,0,0,0"); // Set the GSM module to receive live SMS
+
+    delay(1000); // Delay for 1 second
 }
