3e4943e6809168c225cac3dfe7da19f9e2b730bd
Redox1223 <147227378+Redox1223@users.noreply.github.com>
2023-11-18 12:16:42 +0530

    Create sound_sensor_test.ino
    Updated test code.

---
+++ SoundSensor/sound_sensor/sound_sensor_test.ino
@@ -0,0 +1,12 @@
+#define sensorPin A0
+
+void setup() {
+
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+}
