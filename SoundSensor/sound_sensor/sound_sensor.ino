8a562be003b1f9428e324dd0eb93a1d3f42353b4
Redox1223 <147227378+Redox1223@users.noreply.github.com>
2023-11-18 12:12:44 +0530

    Update sound_sensor.ino
    updated,
    New code

--- SoundSensor/sound_sensor/sound_sensor.ino
+++ SoundSensor/sound_sensor/sound_sensor.ino
@@ -1,29 +1,73 @@
-#define sensorPin 8
+#define LED_1 2
+#define LED_2 3
+#define LED_3 4
+#define LED_4 5
+#define LED_5 6
+#define sensorPin A0
 
-// Variable to store the time when last event happened
+bool ledPin1Status;
+bool ledPin2Status;
+bool ledPin3Status;
+bool ledPin4Status;
+bool ledPin5Status;
 
-unsigned long lastEvent = 0;
-
-void setup(){
-    pinMode(sensorPin,INPUT); // Set sensor pin as an INPUT
-    Serial.begin(9600);
-
+void setup() {
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(LED_4, OUTPUT);
+  pinMode(LED_5, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
 }
 
-void loop(){
-    //Read Sound Sensor
-    int sensorData = digitalRead(sensorPin);
+void loop() {
+  int sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
 
-    // If pin goes LOW,sound is detected
-    if (sensorData == LOW){
+  if (sensorValue > 200)
+    ledPin1Status = 1;
+  if (sensorValue > 250)
+    ledPin2Status = 1;
+  if (sensorValue > 300)
+    ledPin3Status = 1;
+  if (sensorValue > 350)
+    ledPin4Status = 1;
+  if (sensorValue > 400)
+    ledPin5Status = 1;
 
-        // if 25 have passed since last LOW state,it means that clap is detected and not due to any spurious sound
-        if(millis()-lastEvent> 25){
-            Serial.println("Clap detected!");
-        }
-
-        // Remember when last event happened
-        lastEvent= millis();
-    
+  if (ledPin1Status == 1 || ledPin2Status == 1 || ledPin3Status == 1 || ledPin4Status == 1 || ledPin5Status == 1) {
+    if (sensorValue > 50 && sensorValue < 400)
+      digitalWrite(LED_1, HIGH);
+    if (sensorValue > 100 && sensorValue < 400)
+      digitalWrite(LED_2, HIGH);
+    if (sensorValue > 200 && sensorValue < 400)
+      digitalWrite(LED_3, HIGH);
+    if (sensorValue > 300 && sensorValue < 400)
+      digitalWrite(LED_4, HIGH);
+    if (sensorValue > 450 && sensorValue > 400) {
+      digitalWrite(LED_5, HIGH);
+      analogWrite(LED_1, 255);  // Set led brightness to maximum and turn on all leds at the same time when reached maximum pitch.
+      analogWrite(LED_2, 255);
+      analogWrite(LED_3, 255);
+      analogWrite(LED_4, 255);
+      analogWrite(LED_5, 255);
     }
+
+    delay(200);
+
+    ledPin5Status = 0;
+    ledPin4Status = 0;
+    ledPin3Status = 0;
+    ledPin2Status = 0;
+    ledPin1Status = 0;
+  }
+
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, LOW);
+  digitalWrite(LED_4, LOW);
+  digitalWrite(LED_5, LOW);
 }
+
+:)
